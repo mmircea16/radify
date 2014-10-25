0bc143338bfc99c9274d9b9546513e9fbd8e674b
heyestom <heyestom@gmail.com>
2014-10-25 17:49:13 +0100

    fixed restarting the dam application server

--- deployment/deploy_api.sh
+++ deployment/deploy_api.sh
@@ -1,3 +1,2 @@
-ssh -i /var/go/ssh_key root@104.131.74.12   'pkill java ; java -jar /api/radify-0.1.0-SNAPSHOT-standalone.jar & ' 
-
+ssh -i /var/go/ssh_key root@104.131.74.12 'pkill java ; nohup java -jar /api/radify-0.1.0-SNAPSHOT-standalone.jar > /dev/null 2>/dev/null </dev/null  &'
 
