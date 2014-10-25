ac299e491ade00e0c91d9960f8739b294e4cd7b9
heyestom <heyestom@gmail.com>
2014-10-25 16:02:04 +0100

    pablo and tom run the server remotly

--- deployment/compile_api.sh
+++ deployment/compile_api.sh
@@ -5,4 +5,3 @@ lein uberjar
 scp -i /var/go/ssh_key -r target/uberjar/radify-0.1.0-SNAPSHOT-standalone.jar  root@104.131.74.12:/api/
 
 
-
---
+++ deployment/deploy_api.sh
@@ -0,0 +1,3 @@
+ssh -i /var/go/ssh_key 'java -jar /api/radify-0.1.0-SNAPSHOT-standalone.jar'
+
+
