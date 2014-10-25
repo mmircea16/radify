9150ddd463f4d934d880b612df31d552fcae1c03
heyestom <heyestom@gmail.com>
2014-10-25 15:44:10 +0100

    fixed path in compile script

--- deployment/compile_api.sh
+++ deployment/compile_api.sh
@@ -2,7 +2,7 @@ sudo apt-get install leiningen
 
 lein uberjar 
 
-scp -i /var/go/ssh_key -r ./target/radify-0.1.0-SNAPSHOT-standalone.jar  root@104.131.74.12:/api/
+scp -i /var/go/ssh_key -r target/uberjar/radify-0.1.0-SNAPSHOT-standalone.jar  root@104.131.74.12:/api/
 
 
 
