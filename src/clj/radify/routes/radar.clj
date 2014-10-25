6a3ccc057223d9c2fdebda3abf17e2e198c7e72f
portovep <portovep@tcd.ie>
2014-10-25 16:55:44 +0100

    Changed API endpoints to live under /api/

--- src/clj/radify/routes/radar.clj
+++ src/clj/radify/routes/radar.clj
@@ -9,6 +9,6 @@
   radar/dummy-radar)
 
 (defroutes app-routes
-  (GET "/" [] "<h1>Hello World! This is radify!</h1>")
-  (GET "/radar/:id" [id] (ring-response/response (get-radar id)))
+  (GET "/api" [] "<h1>Hello World! This is radify API!</h1>")
+  (GET "/api/radar/:id" [id] (ring-response/response (get-radar id)))
   (route/not-found "<h1>Page not found</h1>"))
