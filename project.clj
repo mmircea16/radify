dc62481e4bb01a2bda2b4952c0c6400e42efeabb
portovep <portovep@tcd.ie>
2014-10-25 13:12:46 +0100

    Added lein config

---
+++ project.clj
@@ -0,0 +1,22 @@
+(defproject radify "0.1.0-SNAPSHOT"
+  :description "radify"
+  :url ""
+
+  :dependencies [[org.clojure/clojure "1.6.0"]
+
+                 [prismatic/schema "0.3.0"]
+                 [compojure "1.1.9"] ;; for routing
+                 [ring "1.3.1"]
+                 [ring/ring-json "0.2.0"] ;; for serializing
+                 [http-kit "2.1.19"]]
+
+  :min-lein-version "2.0.0"
+
+  :source-paths ["src/clj"]
+  :resource-paths ["public"]
+
+  :main ^:skip-aot radify.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}}
+             :dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                                  [ring-mock "0.1.5"]]})
