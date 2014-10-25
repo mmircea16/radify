70b139f12fdeee2aafb5ece669d58557d2913563
Mircea Moise <mmoise@thoughtworks.com>
2014-10-25 13:24:28 +0100

    Mircea: a very colorful radar :))

--- public/js/radar.js
+++ public/js/radar.js
@@ -2,18 +2,44 @@
 var radar = radar || {};
 
 radar.painter = function() {
-
     'use strict';
-    return {
+
+     var tiers = [
+         {
+             radius: 100,
+             color: 'red'
+         },
+         {
+             radius: 200,
+             color: 'blue'
+         },
+         {
+             radius: 300,
+             color: 'yellow'
+         },
+         {
+             radius: 400,
+             color: 'green'
+         }
+     ];
+
+     function sort_tiers_in_order_of_radius(tiers) {
+         return tiers.sort(function(a_tier, another_tier){
+             return another_tier.radius - a_tier.radius;
+         })
+     }
+
+     return {
         apply_to_page: function() {
             var svg = d3.select("svg");
             var circle = svg.selectAll("circle")
-                .data([100])
+                .data(sort_tiers_in_order_of_radius(tiers))
                 .enter()
                 .append("circle")
-                .attr("cx", 100)
-                .attr("cy", 100)
-                .attr("r", function(d) { return d;})
+                .attr("cx", 400)
+                .attr("cy", 400)
+                .attr("r", function(tier) { return tier.radius;})
+                .attr("fill", function(tier) { return tier.color;})
         }
     }
 
