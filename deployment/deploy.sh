8e7e2873b0d6238951c2d7849e5530248afe65e7
portovep <portovep@tcd.ie>
2014-10-25 20:11:50 +0100

    Changed static resources route in deployment script

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,3 +1,3 @@
-echo "Hello world"
+echo "deploying static site..."
 
-scp -i /var/go/ssh_key -r ./public/* root@104.131.74.12:/usr/share/nginx/
+scp -i /var/go/ssh_key -r ./resouces/public/* root@104.131.74.12:/usr/share/nginx/
