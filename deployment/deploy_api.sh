61622c04fe4649155f47e5a2a2847cab8b70721a
heyestom <heyestom@gmail.com>
2014-10-25 17:30:54 +0100

    now kill off the app server

--- deployment/deploy_api.sh
+++ deployment/deploy_api.sh
@@ -1,3 +1,3 @@
-ssh -i /var/go/ssh_key root@104.131.74.12   'java -jar /api/radify-0.1.0-SNAPSHOT-standalone.jar & ' 
+ssh -i /var/go/ssh_key root@104.131.74.12   'pkill java ; java -jar /api/radify-0.1.0-SNAPSHOT-standalone.jar & ' 
 
 
