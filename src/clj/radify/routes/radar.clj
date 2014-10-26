611e9dea7e9a4fd24f1071175d959af94d265192
portovep <portovep@tcd.ie>
2014-10-26 01:07:57 +0100

    Moved some logic from routes to a controller

--- src/clj/radify/routes/radar.clj
+++ src/clj/radify/routes/radar.clj
@@ -1,33 +1,15 @@
 (ns radify.routes.radar
-  (:require [radify.models.radar :as radar]
-            [radify.db.couchdb :as db]
+  (:require [radify.controllers.radar :as controller]
             [compojure.core :refer [GET POST defroutes context]]
-            [compojure.route :as route]
-            [ring.util.response :as ring]))
+            [compojure.route :as route]))
 
-(defn- response-wrapper [status data]
-  (ring/status (ring/response data) status))
-
-(def ^:private error-saving
-  (response-wrapper 400 {:message "Oops, something bad ocurred while trying to save"}))
-
-(defn get-radar [id]
-  (if (= id "dummy")
-    radar/dummy-radar
-    (db/load-item id)))
-
-(defn save-radar [radar]
-  (let [result (db/save-item radar)]
-      (if (nil? result)
-          error-saving
-          (ring/response result))))
 
 (defroutes app-routes
-    (GET "/" [] (ring/resource-response "index.html" {:root "public/html"}))
+    (GET "/" [] (controller/main-page))
     (route/resources "/")
 
-    (GET "/api" [] "<h1>Hello World! This is radify API!</h1>")
-    (GET "/api/radar/:id" [id] (ring/response (get-radar id)))
-    (POST "/api/radar" {body :body} (save-radar body))
+    (GET "/api" [] (controller/api-welcome))
+    (GET "/api/radar/:id" [id] (controller/get-radar id))
+    (POST "/api/radar" {body :body} (controller/save-radar body))
 
     (route/not-found "<h1>Page not found</h1>"))
