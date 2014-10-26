609a32de99041c09b383353208341b5bae8769fa
heyestom <heyestom@gmail.com>
2014-10-26 05:26:37 +0000

    fixed typo in deolp script

--- deployment/deploy_api.sh
+++ deployment/deploy_api.sh
@@ -1,6 +1,6 @@
 scp -i /var/go/ssh_key -r ./app.rb root@104.131.74.12:/radify/
 scp -i /var/go/ssh_key -r ./Gemfile root@104.131.74.12:/radify/
-scp -i /var/go/ssh_key -r ./config.ruroot@104.131.74.12:/radify/
+scp -i /var/go/ssh_key -r ./config.ru root@104.131.74.12:/radify/
 
 ssh -i /var/go/ssh_key root@104.131.74.12 ' pkill ruby ; unicorn /radify/config.ru  &'
 
