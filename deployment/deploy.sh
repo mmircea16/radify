1ea16e7898a57cc60f1210d90acd3d615835affe
Mircea Moise <mmoise@thoughtworks.com>
2014-10-26 06:24:19 +0000

    Mircea: Copying the resources in the radify folder also

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,3 +1,4 @@
 echo "deploying static site..."
 
 scp -i /var/go/ssh_key -r ./resources/public/* root@104.131.74.12:/usr/share/nginx/
+scp -i /var/go/ssh_key -r ./resources/public/* root@104.131.74.12:/radify/
