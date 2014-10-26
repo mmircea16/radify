c70d4042d2b0c8f94e085cc0f35c08a314e7fbcd
Mircea Moise <mmoise@thoughtworks.com>
2014-10-26 06:27:07 +0000

    Mircea: Yet another small tweak

--- deployment/deploy_api.sh
+++ deployment/deploy_api.sh
@@ -2,5 +2,5 @@ scp -i /var/go/ssh_key -r ./app.rb root@104.131.74.12:/radify/
 scp -i /var/go/ssh_key -r ./Gemfile root@104.131.74.12:/radify/
 scp -i /var/go/ssh_key -r ./config.ru root@104.131.74.12:/radify/
 
-ssh -i /var/go/ssh_key root@104.131.74.12 'pkill unicorn ; nohup CONSUMER_KEY=$CONSUMER_KEY CONSUMER_SECRET=$CONSUMER_SECRET  unicorn /radify/config.ru > /dev/null 2> /dev/null </dev/null &  '
+ssh -i /var/go/ssh_key root@104.131.74.12 'pkill unicorn ; CONSUMER_KEY=$CONSUMER_KEY CONSUMER_SECRET=$CONSUMER_SECRET nohup unicorn /radify/config.ru > /dev/null 2> /dev/null </dev/null &  '
 
